6a65b35fd73a3563cfd351dcbfd4abaf75b97f07
markinascimento <markinhonascimento50@gmail.com>
2024-05-26 17:20:50 -0300

    test: new test to select component

--- src/components/Select/index.spec.tsx
+++ src/components/Select/index.spec.tsx
@@ -1,26 +1,62 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Select } from ".";
 
 const options = [
   { label: "value 1", code: "1" },
   { label: "value 2", code: "2" },
   { label: "value 3", code: "3" },
-  { label: "value 4", code: "4" },
-  { label: "value 5", code: "5" },
 ];
 
 describe("<Select />", () => {
   test("Should render select in the document", () => {
-    const { getByLabelText } = render(
+    const handleChange = jest.fn();
+
+    const { getByTestId } = render(
       <Select
-        label="Testing"
-        onChangeValue={jest.fn()}
-        options={options}
         value=""
+        label="Testing"
+        options={options}
+        onChangeValue={handleChange}
       />
     );
 
-    expect(getByLabelText("Testing")).toBeInTheDocument();
+    expect(getByTestId("select-id")).toBeInTheDocument();
+  });
+
+  test("Should not render component when disabled", () => {
+    const handleChange = jest.fn();
+
+    const { getByTestId } = render(
+      <Select
+        value=""
+        isDisabled
+        label="Testing"
+        onChangeValue={handleChange}
+        options={options}
+      />
+    );
+
+    fireEvent.click(getByTestId("select-id"));
+
+    expect(handleChange).not.toHaveBeenCalled();
+  });
+
+  test("Should render values in the document", async () => {
+    const onChangeValueMock = jest.fn();
+
+    render(
+      <Select
+        label="Select Label"
+        value=""
+        options={options}
+        onChangeValue={onChangeValueMock}
+      />
+    );
+
+    fireEvent.mouseDown(screen.getByRole("combobox"));
+    fireEvent.click(screen.getByText("value 1"));
+
+    expect(screen.getByText("value 1")).toBeInTheDocument();
   });
 });
